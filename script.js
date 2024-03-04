62ccf73e4b972cacbb043ac9211bbc563fd3502a
Kishore J <133196824+Johnkishore11@users.noreply.github.com>
2024-03-04 13:22:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const btn=document.getElementById('calculate');
+let resultArea = document.querySelector(".comment");
+btn.addEventListener('click',function(){
+    let height = document.querySelector('#height').value;
+    let weight = document.querySelector('#weight').value;
+
+    if(height == "" || weight==""){
+        alert('Please fill the input fields!');
+        return;
+    }
+
+    height= height/100
+
+    let BMI = (weight/(height*height));
+
+    BMI= BMI.toFixed(2);
+    
+    document.querySelector("#result").innerHTML=BMI;
+
+    let status="";
+    if(BMI<18.5){
+        status="Underweight";
+    }
+    if(BMI>=18.5 && BMI<25){
+        status="Healthy";
+    }
+    if(BMI>=30){
+        status="Obese";
+    }
+
+
+    document.querySelector("#comment").innerHTML=`You are <span 
+    id="comment">${status}</span>`;
+
+});
